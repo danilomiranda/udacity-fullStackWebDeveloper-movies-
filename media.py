3977bceee8ded369aa7e18c1b440ad44a2545bfc
Danilo Miranda <danilokassio@gmail.com>
2016-12-22 00:12:19 -0300

    Transfer some atributes from parent to child class

--- media.py
+++ media.py
@@ -6,5 +6,6 @@ class Movie(video.Video):
     VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']
     def __init__(self, movie_title, movie_storyline, poster_image,
                 trailer_youtube):
-        video.Video.__init__(self,movie_title, movie_storyline, poster_image,
-                    trailer_youtube)
+        video.Video.__init__(self,movie_title, movie_storyline)
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url =trailer_youtube
