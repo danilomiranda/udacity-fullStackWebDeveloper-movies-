6054316c22c59a734964cefd75c9b881e87552dd
Danilo Miranda <danilokassio@gmail.com>
2017-01-06 22:24:36 -0300

    refactor

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,12 +3,12 @@ import fresh_tomatoes
 # initialize Toy Story
 toy_story = media.Movie("Toy Story",
                         "A story of a boy an his toys tha come to life",
-                        "https://upload.wikimedia.org/wikipedia/pt/d/dc/Movie_poster_toy_story.jpg",
+                        "https://upload.wikimedia.org/wikipedia/pt/d/dc/Movie_poster_toy_story.jpg", # noqa
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 # initialize Avatar
 avatar = media.Movie("Avatar", "A marine on a alien planet",
-                        "https://upload.wikimedia.org/wikipedia/pt/b/b0/Avatar-Teaser-Poster.jpg",
+                        "https://upload.wikimedia.org/wikipedia/pt/b/b0/Avatar-Teaser-Poster.jpg", # noqa
                         "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 # initialize Elite Squad
@@ -32,8 +32,15 @@ the_shawshank_redemption = media.Movie("The Shawshank Redemption", "In 1947 \
                             "https://www.youtube.com/watch?v=6hB3S9bIaco")
 
 # initialize The Perks Of Being a WallFlower
-the_perks_of_being_a_wallflower = media.Movie("The Perks of Being a Wallflower", "The film is set against the background of a young student, Charlie, who has been suffering from clinical depression from childhood setbacks and has recently been discharged from a mental health care institution to begin his adaptation to a normal lifestyle as a young high school student. ",
-                            "https://upload.wikimedia.org/wikipedia/en/0/0b/The_Perks_of_Being_a_Wallflower_Poster.jpg",
+the_perks_of_being_a_wallflower = media.Movie("The Perks of Being a Wallflower",
+                            "The film is set against the background of a young\
+                             student, Charlie, who has been suffering from\
+                          clinical depression from childhood setbacks and\
+                           has recently been discharged from a mental\
+                            health care institution to begin his adaptation\
+                             to a normal lifestyle as a young high\
+                              school student. ",
+                            "https://upload.wikimedia.org/wikipedia/en/0/0b/The_Perks_of_Being_a_Wallflower_Poster.jpg", # noqa
                             "https://www.youtube.com/watch?v=n5rh7O4IDc0")
 
 # Put all toguether in a array
